e4de83b62e40d9976cf4eeb3e1d4ac9bb48a7a05
teixeirajhon <jhonatan93_@hotmail.com>
2025-08-05 23:28:37 -0300

    First Commit

---
+++ src/Pikachu.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Pikachu() {
+  return (
+    <div>
+      <h2>
+        Todos os Pokemon 
+      </h2>
+      <p>
+        Cores do Pokemon
+      </p>
+    </div>
+  );
+}
+
+export default Pikachu;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './Login';
+import Pikachu from './Pikachu';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Pikachu />
+    <Login />
   </React.StrictMode>
 );
 
